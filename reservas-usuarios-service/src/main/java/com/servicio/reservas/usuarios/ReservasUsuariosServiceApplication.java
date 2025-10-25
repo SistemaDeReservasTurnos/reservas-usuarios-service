8ecb386386f9cdd6a69fb812672acbfce15fc1fc
Nicolás Rodriguez <117177426+NiKolas1828@users.noreply.github.com>
2025-10-25 15:44:04 -0500

    config bd

--- reservas-usuarios-service/src/main/java/com/servicio/reservas/usuarios/ReservasUsuariosServiceApplication.java
+++ reservas-usuarios-service/src/main/java/com/servicio/reservas/usuarios/ReservasUsuariosServiceApplication.java
@@ -1,12 +1,14 @@
 package com.servicio.reservas.usuarios;
-
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class ReservasUsuariosServiceApplication {
 
+
     public static void main(String[] args) {
+        Dotenv.configure().systemProperties().load();
         SpringApplication.run(ReservasUsuariosServiceApplication.class, args);
     }
 
