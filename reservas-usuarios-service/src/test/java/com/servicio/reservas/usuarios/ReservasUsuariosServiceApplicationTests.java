a0fab1d93e47cfbe595d4083c21ffe3995da1947
Nicolás Rodriguez <117177426+NiKolas1828@users.noreply.github.com>
2025-10-25 15:12:41 -0500

    carpetas iniciales

---
+++ reservas-usuarios-service/src/test/java/com/servicio/reservas/usuarios/ReservasUsuariosServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.servicio.reservas.usuarios;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReservasUsuariosServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
