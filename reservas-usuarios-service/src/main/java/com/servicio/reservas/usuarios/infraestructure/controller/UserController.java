de4ada0c36ff7dd1c4962a038776accebf878528
Juan Miguel <juan.miguel.palacios@correounivalle.edu.co>
2025-11-16 01:21:21 -0500

    feat(controller): gestión de roles en los endpoints

--- reservas-usuarios-service/src/main/java/com/servicio/reservas/usuarios/infraestructure/controller/UserController.java
+++ reservas-usuarios-service/src/main/java/com/servicio/reservas/usuarios/infraestructure/controller/UserController.java
@@ -9,6 +9,7 @@ import com.servicio.reservas.usuarios.domain.entities.Role;
 import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -23,30 +24,35 @@ public class UserController {
     }
 
     @PostMapping("/create")
+    @PreAuthorize("hasAuthority('SCOPE_SCOPE_INTERNAL_SERVICE') or hasAuthority('ROLE_ADMINISTRADOR')")
     public ResponseEntity<UserResponse> createUser(@Valid @RequestBody UserRequest userRequest) {
         UserResponse userResponse = userService.createuser(userRequest);
         return ResponseEntity.status(HttpStatus.CREATED).body(userResponse);
     }
     
     @GetMapping("/role/{role}")
+    @PreAuthorize("hasAuthority('ROLE_ADMINISTRADOR')")
     public ResponseEntity<List<UserResponse>> getAllByRole(@PathVariable Role role) {
         List<UserResponse> users = userService.getAllByRole(role.name().toLowerCase());
         return ResponseEntity.ok().body(users);
     }
 
     @GetMapping("/email/{email}")
+    @PreAuthorize("hasAuthority('SCOPE_SCOPE_INTERNAL_SERVICE') or hasAuthority('ROLE_ADMINISTRADOR') or authentication.name == #email")
     public ResponseEntity<UserResponse> getUserByEmail(@PathVariable String email){
             UserResponse user = userService.getUserByEmail(email);
             return ResponseEntity.ok().body(user);
     }
 
     @PutMapping("/deactivate/{email}")
+    @PreAuthorize("hasAuthority('ROLE_ADMINISTRADOR')")
     public ResponseEntity<String> deactivateUser(@PathVariable String email) {
             userService.deactivateUser(email);
             return ResponseEntity.ok("User successfully deactivated");
     }
 
     @PutMapping("/update/{email}/{column}/{value}")
+    @PreAuthorize("hasAuthority('ROLE_ADMINISTRADOR') or authentication.name == #email")
     public ResponseEntity<String> updateUser(
              @PathVariable String email,
              @PathVariable String column,
@@ -56,6 +62,7 @@ public class UserController {
     }
 
     @PutMapping("/update-password/{email}")
+    @PreAuthorize("authentication.name == #email")
     public ResponseEntity<String> updatePassword(
             @PathVariable String email,
             @Valid @RequestBody UpdatePasswordRequest request) {
@@ -64,6 +71,7 @@ public class UserController {
     }
 
     @PutMapping("/update-email/{email}")
+    @PreAuthorize("authentication.name == #email")
     public ResponseEntity<String> updateEmail(
             @PathVariable String email,
             @Valid @RequestBody UpdateEmailRequest request) {
