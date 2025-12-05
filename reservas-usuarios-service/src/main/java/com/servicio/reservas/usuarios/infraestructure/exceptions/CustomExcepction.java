4dd89aa8896f4de0a3042bdefa7469b97d8434ac
Nicolas Rodriguez <nicolas.rodriguez.romero@correounivalle.edu.co>
2025-12-05 02:33:10 -0500

    fix: corrección manejo de excepciones

--- reservas-usuarios-service/src/main/java/com/servicio/reservas/usuarios/infraestructure/exceptions/CustomExcepction.java
+++
@@ -1,7 +0,0 @@
-package com.servicio.reservas.usuarios.infraestructure.exceptions;
-
-public class CustomExcepction extends RuntimeException{
-    public CustomExcepction(String message) {
-        super(message);
-    }
-}
