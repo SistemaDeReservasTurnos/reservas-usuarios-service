dde293867439fdf7d2b155f0e2419640595b9b51
sfcg2 <castillo.sofia@correounivalle.edu.co>
2025-12-05 22:42:41 -0500

    archivos de idea

--- com/servicio/reservas/usuarios/infraestructure/exceptions/BusinessException.java
+++
@@ -1,7 +0,0 @@
-package com.servicio.reservas.usuarios.infraestructure.exceptions;
-
-public class BusinessException extends RuntimeException {
-    public BusinessException(String message) {
-        super(message);
-    }
-}
